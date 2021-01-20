76ff2426075e1357814246b010f2855caee1d8dc
flohrel <flohrel@student.42.fr>
2021-01-20 17:19:56 +0100

    TODO : fix type == -1 + fix signed integers (placement signe '-')

--- src/set_int.c
+++ src/set_int.c
@@ -1,11 +1,32 @@
 #include "printf.h"
 
-char			*set_int(va_list *args, t_param *arg)
+char		*signed_nb_format(t_param *arg, char *tmp)
+{
+	int			tmp_len;
+	int			str_len;
+	char		*nb_str;
+	t_bool		is_neg;
+
+	is_neg = FALSE;
+	if (*tmp == '-')
+	{
+		is_neg = TRUE;
+	}
+	tmp_len = number_format(arg, &tmp, ft_strlen(tmp));
+	if (tmp_len == -1)
+		return (NULL);
+	str_len = n_str_alloc(arg, &nb_str, tmp_len);
+	if (str_len == -1)
+		return (NULL);
+	if (!CHK_FLAG(arg->flags, LEFT))
+		ft_memcpy(nb_str + (str_len - tmp_len), tmp, tmp_len);
+	ft_memcpy(nb_str, tmp, tmp_len);
+	return (nb_str);
+}
+
+char		*set_int(va_list *args, t_param *arg)
 {
 	int			nb;
-	int			str_len;
-	int			tmp_len;
-	size_t		index;
 	char		*nb_str;
 	char		*tmp;
 
@@ -15,15 +36,7 @@ char			*set_int(va_list *args, t_param *arg)
 	tmp = ft_itoa(nb);
 	if (!tmp)
 		return (NULL);
-	tmp_len = number_format(arg, &tmp, ft_strlen(tmp));
-	if (tmp_len == -1)
-		return (NULL);
-	str_len = n_str_alloc(arg, &nb_str, tmp_len);
-	if (str_len == -1)
-		return (NULL);
-	if (!CHK_FLAG(arg->flags, LEFT))
-		index = str_len - tmp_len;
-	ft_memcpy(nb_str + index, tmp, tmp_len);
+	nb_str = signed_nb_format(arg, tmp);
 	free(tmp);
 	return (nb_str);
 }
