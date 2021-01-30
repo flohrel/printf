3a46be534bb7c0a0973bdabace02ef90efec8dbe
flohrel <flohrel@student.42.fr>
2021-01-30 18:50:24 +0100

    convert function renamed

---
+++ src/format/format_utils.c
@@ -0,0 +1,125 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   format_utils.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flohrel <flohrel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/25 12:23:53 by flohrel           #+#    #+#             */
+/*   Updated: 2021/01/30 12:42:23 by flohrel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	set_int(va_list *args, t_param *arg)
+{
+	long		nb;
+	char		*tmp;
+	int			index;
+	int			tmp_len;
+	t_bool		is_neg;
+
+	is_neg = FALSE;
+	nb = va_arg(*args, int);
+	index = 0;
+	if ((nb < 0) || check_flag(arg->flags, SIGN) || check_flag(arg->flags, BLANK))
+	{
+		if ((nb < 0) && (is_neg = TRUE))
+			nb = -nb;
+		index = 1;
+	}
+	if (!(tmp = ft_ltoa(nb)) ||
+		(tmp_len = number_format(arg, &tmp, ft_strlen(tmp))) == -1)
+		return ;
+	if (check_flag(arg->flags, WIDTH) && (arg->width > tmp_len) &&
+		!check_flag(arg->flags, LEFT))
+		index = arg->width - tmp_len;
+	ft_memcpy(arg->buffer + index, tmp, tmp_len);
+	if (is_neg || check_flag(arg->flags, SIGN) || check_flag(arg->flags, BLANK))
+		set_sign(arg, index, is_neg);
+	free(tmp);
+}
+
+void	set_uint(va_list *args, t_param *arg)
+{
+	unsigned int	nb;
+	int				tmp_len;
+	size_t			index;
+	char			*tmp;
+
+	nb = (unsigned int)va_arg(*args, int);
+	tmp = ft_ltoa(nb);
+	if (!tmp)
+		return ;
+	tmp_len = number_format(arg, &tmp, ft_strlen(tmp));
+	if (tmp_len == -1)
+		return ;
+	index = 0;
+	if (check_flag(arg->flags, WIDTH) && (arg->width > tmp_len) &&
+		!check_flag(arg->flags, LEFT))
+		index = arg->width - tmp_len;
+	ft_memcpy(arg->buffer + index, tmp, tmp_len);
+	free(tmp);
+}
+
+void	set_ptr(va_list *args, t_param *arg)
+{
+	unsigned long	ptr_addr;
+	char			*tmp;
+	int				tmp_len;
+	size_t			index;
+
+	ptr_addr = (unsigned long)va_arg(*args, void *);
+	tmp = ft_ultoa_base(ptr_addr, "0123456789abcdef");
+	if (!tmp)
+		return ;
+	tmp_len = number_format(arg, &tmp, ft_strlen(tmp));
+	if (tmp_len == -1)
+		return ;
+	index = 2;
+	if (check_flag(arg->flags, WIDTH) && (arg->width > tmp_len + 2) &&
+		!check_flag(arg->flags, LEFT))
+		index = arg->width - tmp_len;
+	ft_memcpy(arg->buffer + index, tmp, tmp_len);
+	set_prefix(arg, index - 2);
+	if (tmp)
+		free(tmp);
+}
+
+void	set_str(va_list *args, t_param *arg)
+{
+	char	*str;
+	int		str_len;
+	size_t	index;
+
+	str = va_arg(*args, char *);
+	str_len = 0;
+	if (str)
+		str_len = ft_strlen(str);
+	else
+		str_len = 6;
+	if (check_flag(arg->flags, PREC) && (arg->precision < str_len))
+		str_len = arg->precision;
+	index = 0;
+	if (!check_flag(arg->flags, LEFT) && check_flag(arg->flags, WIDTH) &&
+		(arg->width > str_len))
+		index = arg->width - str_len;
+	if (str)
+		ft_memcpy(arg->buffer + index, str, str_len);
+	else
+		ft_memcpy(arg->buffer + index, NULLSTR, str_len);
+}
+
+void	set_char(va_list *args, t_param *arg)
+{
+	unsigned char	c;
+
+	c = (unsigned char)va_arg(*args, int);
+	if (!arg->width)
+		arg->width++;
+	if (check_flag(arg->flags, LEFT))
+		*(arg->buffer) = c;
+	else
+		*(arg->buffer + arg->width - 1) = c;
+}
---
+++ src/format/format_utils2.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   format_utils2.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: flohrel <flohrel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/01/25 12:55:28 by flohrel           #+#    #+#             */
+/*   Updated: 2021/01/30 12:42:16 by flohrel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	set_sign(t_param *arg, int index, t_bool is_neg)
+{
+	char	c;
+
+	if (is_neg)
+		c = '-';
+	else
+	{
+		if (check_flag(arg->flags, BLANK))
+			c = ' ';
+		else
+			c = '+';
+	}
+	if (check_flag(arg->flags, ZERO) && !(check_flag(arg->flags, PREC)))
+		*(arg->buffer) = c;
+	else
+		*(arg->buffer + (index - 1)) = c;
+}
+
+void	set_prefix(t_param *arg, int index)
+{
+	if (arg->type == 7)
+		ft_memcpy(arg->buffer + index, "0X", 2);
+	else
+		ft_memcpy(arg->buffer + index, "0x", 2);
+}
+
+void	set_hex(va_list *args, t_param *arg)
+{
+	unsigned int	nb;
+	int				tmp_len;
+	size_t			index;
+	char			*tmp;
+
+	nb = (unsigned int)va_arg(*args, int);
+	if (arg->type == 6)
+		tmp = ft_ltoa_base(nb, "0123456789abcdef");
+	else
+		tmp = ft_ltoa_base(nb, "0123456789ABCDEF");
+	if (!tmp)
+		return ;
+	tmp_len = number_format(arg, &tmp, ft_strlen(tmp));
+	if (tmp_len == -1)
+		return ;
+	index = 0;
+	if (check_flag(arg->flags, ALT) && nb)
+		index = 2;
+	if (check_flag(arg->flags, WIDTH) && (arg->width > tmp_len) &&
+		!check_flag(arg->flags, LEFT))
+		index = arg->width - tmp_len;
+	ft_memcpy(arg->buffer + index, tmp, tmp_len);
+	if (check_flag(arg->flags, ALT) && nb)
+		set_prefix(arg, index - 2);
+	free(tmp);
+}
+
+void	set_notype(va_list *args, t_param *arg)
+{
+	int	index;
+
+	index = 0;
+	(void)args;
+	if (check_flag(arg->flags, WIDTH) && (arg->width > 1) &&
+		!check_flag(arg->flags, LEFT))
+		index = arg->width - 1;
+	*(arg->buffer + index) = '%';
+}
+
+void	set_count(va_list *args, int count)
+{
+	int	*count_ptr;
+
+	count_ptr = va_arg(*args, int *);
+	*count_ptr = count;
+}
